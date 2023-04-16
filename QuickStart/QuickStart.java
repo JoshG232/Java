dadab34b44360643c9a1b344d84ebfa56f3ba275
JoshG232 <jcgeorgiou88@gmail.com>
2023-04-16 14:12:20 +0100

    Extra Hello World

--- QuickStart/QuickStart.java
+++
@@ -1,9 +0,0 @@
-
-package QuickStart;
-
-class QuickStart {
-    public static void main (String[] args){
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-    }
-}
